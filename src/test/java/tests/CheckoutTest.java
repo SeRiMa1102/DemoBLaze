04a8d3096e8520f11e03ad458e8f9ff79fd22cc3
Rinat Setdikov <rmsetdikov@miem.hse.ru>
2025-03-13 10:46:40 +0300

    draft(all) version in pom.xml

--- src/test/java/tests/CheckoutTest.java
+++ src/test/java/tests/CheckoutTest.java
@@ -1,5 +1,7 @@
 package tests;
 
+import io.qameta.allure.Description;
+import io.qameta.allure.Flaky;
 import io.qameta.allure.Link;
 import org.testng.Assert;
 import org.testng.annotations.Test;
@@ -8,6 +10,8 @@ import pages.ProductPage;
 public class CheckoutTest extends BaseTest {
     @Test
     @Link("https://www.demoblaze.com/")
+    @Flaky
+    @Description("All tests")
     public void checkoutFull() {
         String str = "RinatTest";
         String product = "Samsung galaxy s6";
